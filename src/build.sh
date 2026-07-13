395ada3fa67c0a978a299d066393687dc948a693
rposts <rishi.investigate@gmail.com>
2026-07-13 16:29:11 +0000

    Change base image
    Signed-off-by: rposts <rishi.investigate@gmail.com>

--- src/build.sh
+++ src/build.sh
@@ -13,7 +13,6 @@ if [ -f /etc/os-release ]; then
 fi
 current_dir="$PWD"
 echo $DISTRO > .distro_zab.txt
-# Ensure sudo is installed along with utilities to build rpm and deb packages
 apt-get update && apt-get install sudo -y
 bash /tmp/linux-on-ibm-z-scripts/Bazel/${version}/build_bazel.sh -y
 tar cvfz bazel-${version}-linux-s390x.tar.gz -C $PWD/bazel/bazel-bin/src bazel
